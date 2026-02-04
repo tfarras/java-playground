d5a2e3aa704f64cbe7becf3d0c27f4a0c3326daa
Taimoor Farras <farrastaimoor@gmail.com>
2026-02-04 21:26:54 +0200

    chore: init umbra

---
+++ umbra/src/main/java/dev/umbra/server/UmbraApplication.java
@@ -0,0 +1,13 @@
+package dev.umbra.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbraApplication.class, args);
+	}
+
+}
---
+++ umbra/src/test/java/dev/umbra/server/UmbraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umbra.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
